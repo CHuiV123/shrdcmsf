7a29cdea1b0df6840c41b728d5ce43112376f50b
CHuiV123 <108331037+CHuiV123@users.noreply.github.com>
2024-07-05 11:01:26 +0800

    Update streampdf.py

--- streampdf.py
+++ streampdf.py
@@ -233,7 +233,7 @@ def footer():
         image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
               width=px(25), height=px(25)),
         " with ❤️ by ",
-        link("https://www.linkedin.com/in/oyasizakiananta/", "@Oyasi"),
+        link("https://www.linkedin.com/showcase/malaysian-smart-factory-msf/", "@MSF"),
     ]
     layout(*myargs)
 ### ---------------------------------------- Footer ---------------------------------------- ###
